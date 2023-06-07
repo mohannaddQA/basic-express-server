40c633d60b5d672c288f2672ecac863e800343a5
mohannaddQA <qa.mohannad@gmail.com>
2023-06-07 17:51:16 +0000

    finished every thing with testing

--- index.js
+++ index.js
@@ -1,6 +1,3 @@
 "use strict";
-
-require("dotenv").config();
 const server = require("./src/server");
-const port = process.env.PORT || 3002;
-server.start(port);
+server.start(server.port);
--- src/error-handlers/500.js
+++ src/error-handlers/500.js
@@ -2,6 +2,6 @@ module.exports = (error, req, res, next) => {
   res.status(500).send({
     code: 500,
     route: req.path,
-    message: `Server Error: ${error.message}`,
+    message: `Server Error: ${error.message || error}`,
   });
 };
